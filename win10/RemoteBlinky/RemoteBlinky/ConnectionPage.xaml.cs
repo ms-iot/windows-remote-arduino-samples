7fb4ec4f6c295a4e0444a5cbd119aad100604308
Jesse Frush <jesse.frush@gmail.com>
2015-10-08 19:48:58 -0700

    Updated telemetry endpoint in RemoteBlinky to new IoTMaker WRA - General Azure endpoint.

--- win10/RemoteBlinky/RemoteBlinky/ConnectionPage.xaml.cs
+++ win10/RemoteBlinky/RemoteBlinky/ConnectionPage.xaml.cs
@@ -15,9 +15,6 @@ using System.Collections.Generic;
 
 namespace RemoteBlinky
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ConnectionPage : Page
     {
         DispatcherTimer timeout;
